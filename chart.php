3af7d29518fd8f344b640869d37bb99c7533efaf
ikeday <ikeday@ikedaymacmini.local>
2020-08-16 01:33:29 +0900

    update css

--- chart.php
+++ chart.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>usage: localhost//bodyplanner/chart.php?data_entry=1</title>
+<title>usage: localhost//bodyplanner/chart.php?data_entry=0</title>
 </head>
 <body>
 <?php
