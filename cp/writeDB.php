fadf877c5043659579a51e3aec54465de1aee50c
ikeday <ikeday819@gmail.com>
2020-09-07 11:25:35 +0900

    For cellphone camera

---
+++ cp/writeDB.php
@@ -0,0 +1,37 @@
+<?php
+
+// MySQLへ引き渡すデータ
+$url = "localhost";
+$user = "root";
+$pass = "skapara1925";
+$db = "bodyplanner";
+$pdomsg = 'mysql:host=' . $url . ';dbname=' . $db . ';charset=utf8';
+
+global $ret_msg;
+$ret_msg = "Write data successful.";
+
+$sql = $_POST["sqlquery"];
+
+try {
+    $pdo = new PDO($pdomsg, $user, $pass);
+} catch (PDOException $e) {
+    $ret_msg= "Connection error: ".$e->getMessage();
+    //die();
+}
+
+
+try {
+    $statement = $pdo->query($sql);
+} catch (PDOException $e) {
+    $ret_msg= "Written error: ".$e->getMessage();
+    //die();
+}
+
+
+
+// ③ヘッダーの設定
+header('Content-type:application/json; charset=utf8');
+// ④JSON形式にして返却
+echo json_encode($ret_msg);
+
+?>
